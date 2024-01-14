6fbee456c18bb99bdba992c06094145742ac8e22
RamSChoudhary <ramrit10@gmail.com>
2024-01-14 11:56:38 +0530

    Added app gw with ssl

--- versions.tf
+++ versions.tf
@@ -7,7 +7,7 @@ terraform {
   }
 backend "azurerm" {
     resource_group_name  = "rg-lz-tfstate-eastus-001"
-    storage_account_name = "strglzeastus002"
+    storage_account_name = "strglzeastus003"
     container_name       = "terraform"
     key                  = "connecitivity.tfstate"
     use_oidc             = true
